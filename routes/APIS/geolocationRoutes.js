a051d319c005687a0249c0a7d9bea8266f2cfeeb
CesarAbraham0428 <CesarAbraham0428@gmail.com>
2024-11-16 20:02:21 -0600

    Fix: Intento de correccion de rutas de geolocalizacion en geolocationRoutes.js para deploy en Railway

--- routes/APIS/geolocationRoutes.js
+++ routes/APIS/geolocationRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const geolocationController = require('../../controllers/Apis/geolocationController');
+const geolocationController = require('../../controllers/apis/geolocationController');
 
 router.post('/servicios-cercanos', geolocationController.getNearbyServices);
 
